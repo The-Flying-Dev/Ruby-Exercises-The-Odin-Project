d3d754adfc6c8f80829e97fb974d976bb0c303c9
The-Flying-Dev <leandranm@outlook.com>
2023-05-14 03:39:23 +0200

    debugging

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -6,25 +6,35 @@
 # To the top, the first line, where the error occured, and ONLY THEN fix the error
 
 def decrement_smallest_value(nested_array)
-  smallest_value = nested_array.flatten.max
+
+  # simpler steps to accomplish the same result
+  #flattened_array = nested_array.flatten.sort
+  #flattened_array[0] - 1
+
+  smallest_value = nested_array.flatten.max # retrieving the highest integer value
   nested_array.each do |array|
-    array.each do |current_value|
-      if smallest_value > current_value
-        smallest_value = current_valu
+    array.each do |current_value| # iterate through each nested element
+      if smallest_value > current_value # syntax error, missing e at end
+        smallest_value = current_value
       end
     end
   end
-  smallest_value -= 1
+  smallest_value -= 1  
 end
 
 # use the stack trace to debug the following method
 # Don't look at this method prior to running the test
 # Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
+
+  # simpler steps to accomplish the same result
+  #greatest_value = nested_array.flatten.sort
+  #greatest_value[-1] + 1
+
   greatest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value # changed nil to current_value
         greatest_value = current_value
       end
     end
@@ -36,10 +46,10 @@ end
 # Use p and puts in order to find what's wrong with our method
 
 def isogram?(string)
-  original_length = string.length
-  string_array = string.downcase.split
-  unique_length = string_array.uniq.length
-  original_length == unique_length
+  original_length = string.length # length of string parameter
+  string_array = string.downcase.split("") # transform string to lowercase then turns it into an array
+  unique_length = string_array.uniq.length # removes duplicate values from array and finds the length
+  original_length == unique_length # comparison returns boolean
 end
 
 # Can you guess what's next?
@@ -51,6 +61,7 @@ end
 
 def yell_greeting(string)
   name = string
-  name = name.downcase
+  name = name.upcase
   greeting = "WASSAP, #{name}!"
+  greeting
 end
